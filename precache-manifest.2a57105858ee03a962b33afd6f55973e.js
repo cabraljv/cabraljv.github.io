df04eef7b025c0972d2872edd1c968c7a9019f03
cabraljv <joaovictorcabral.dev@gmail.com>
2021-09-28 20:39:07 -0300

    Updates

---
+++ precache-manifest.2a57105858ee03a962b33afd6f55973e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98f1a497546f2a9cf76f4b11d1f5cf7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0af4ebd52f805abcb58b",
+    "url": "./static/css/2.9497e657.chunk.css"
+  },
+  {
+    "revision": "9251a32fadbb9afc6e7a",
+    "url": "./static/css/main.fc61411a.chunk.css"
+  },
+  {
+    "revision": "0af4ebd52f805abcb58b",
+    "url": "./static/js/2.26439c0f.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "./static/js/2.26439c0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9251a32fadbb9afc6e7a",
+    "url": "./static/js/main.f19a30d4.chunk.js"
+  },
+  {
+    "revision": "f48a2f0c3d1c2c26d364",
+    "url": "./static/js/runtime-main.13d74439.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.488469a7964f4e44f4239e694d1f5056.js"
+  "./precache-manifest.2a57105858ee03a962b33afd6f55973e.js"
 );
 
 self.addEventListener('message', (event) => {
