c57f2429f1fc2847ca2c1828df3cce5ac87565b1
cabraljv <joaovictorcabral.dev@gmail.com>
2021-09-28 16:58:47 -0300

    Updates

---
+++ precache-manifest.56eee5c02399d0f4f33dde4a5eb66469.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c4a2bee483462f57c49815a35139310",
+    "url": "/cabraljv.dev/index.html"
+  },
+  {
+    "revision": "f36c9bdeadfdd918ba5b",
+    "url": "/cabraljv.dev/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "178db2d132c1ae11caed",
+    "url": "/cabraljv.dev/static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "f36c9bdeadfdd918ba5b",
+    "url": "/cabraljv.dev/static/js/2.a9742a14.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/cabraljv.dev/static/js/2.a9742a14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "178db2d132c1ae11caed",
+    "url": "/cabraljv.dev/static/js/main.8c0fe6ef.chunk.js"
+  },
+  {
+    "revision": "e823a17c33f5fa8305f1",
+    "url": "/cabraljv.dev/static/js/runtime-main.cd51c9c5.js"
+  }
+]);
